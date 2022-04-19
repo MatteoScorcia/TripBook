c74878ca4cd6976572e70ee5d0908e5bd572afea
Matteo-Aindo <matteo@aindo.com>
2022-04-19 10:58:09 +0200

    refactor useAuth

--- src/app/src/components/PageAuth.tsx
+++ src/app/src/components/PageAuth.tsx
@@ -10,21 +10,21 @@ import {ReactComponent as Logo} from "../svg/logo.svg";
 export default function PageAuth(props: { sign: "login" | "sign-up" }) {
     const { sign } = props;
 
-    const [authError, setAuthError] = useState<[string, string] | undefined>();
-
+    //Navigation history states
     const navigate = useNavigate();
     const location = useLocation();
-
     const from = (location.state as any)?.from?.pathname || "/";
 
+    //AUTH states
+    const { login, isLoginLoading, signup, isSignupLoading, authError } = useAuth();
+    const [showModal, setShowModal] = useState(false);
     const [formData, setFormData] = useState({
         name: "",
         password: "",
         email: "",
     });
 
-    const { login, signup } = useAuth();
-
+    //Handlers
     function handleChange(event: any) {
         const { name, value } = event.target;
         setFormData((prevFormData) => {
@@ -37,33 +37,37 @@ export default function PageAuth(props: { sign: "login" | "sign-up" }) {
 
     function handleSubmit() {
         if (sign === "login") {
-            login(formData.name, formData.password)
+            login({name: formData.name, password: formData.password})
                 .then(() => {
                     navigate(from, { replace: true });
                 })
-                .catch((err) => {
-                    setAuthError([err.message, err.response.data.error]);
+                .catch(() => {
+                    setShowModal(true);
                 });
         } else {
-            signup(formData.name, formData.password, formData.email)
+            signup({name: formData.name, password: formData.password, email: formData.email})
                 .then(() => {
                     navigate(from, { replace: true });
                 })
-                .catch((err) => {
-                    setAuthError([err.message, err.response.data.error]);
+                .catch(() => {
+                    setShowModal(true);
                 });
         }
     }
 
     function handleKeyDown(event: KeyboardEvent<HTMLInputElement>){
         if(event.key === "Enter") {
-            handleSubmit()
+            handleSubmit();
         }
     }
 
     return (
         <div className="flex flex-row" onKeyDown={handleKeyDown}>
-            <div className="w-1/2 flex justify-center items-center">
+            <div
+                className={(isLoginLoading || isSignupLoading) ?
+                    "animate-pulse w-1/2 flex justify-center items-center" :
+                    "w-1/2 flex justify-center items-center"}
+            >
                 <div className="bg-white p-4 flex flex-col items-center shadow-md rounded-md space-y-4">
                     <div className="flex justify-center">
                         <Logo />
@@ -114,24 +118,25 @@ export default function PageAuth(props: { sign: "login" | "sign-up" }) {
                 style={{ backgroundImage: `url(${travelImg})` }}
             />
 
-            {authError && (
-                <Modal
-                    show={true}
-                    actionButtons={
-                        <Button accent={true} onClick={() => setAuthError(undefined)}>
-                            <span>Close</span>
-                        </Button>
-                    }
-                >
-                    <div className="flex justify-center">
-                        <img src={warning} alt={"warning"} className="w-16 h-16" />
-                    </div>
-                    <div className="mt-4">
-                        <div>{authError[0]}</div>
-                        {authError[1] && <div className="text-medium">{authError[1]}</div>}
-                    </div>
-                </Modal>
-            )}
+            {/* Auth Error Modal */}
+            <Modal
+                show={showModal}
+                onClickOutsideModal={() => setShowModal(false)}
+                actionButtons={
+                    <Button accent={true} onClick={() => setShowModal(false)}>
+                        <span>Close</span>
+                    </Button>
+                }
+            >
+                <div className="flex justify-center">
+                    <img src={warning} alt={"warning"} className="w-16 h-16" />
+                </div>
+                <div className="mt-4">
+                    <div>{authError?.message}</div>
+                    <div className="text-medium">{authError?.response?.data?.error}</div>
+                </div>
+            </Modal>
+
         </div>
     );
 }
--- src/app/src/customHooks/useAuth.ts
+++ src/app/src/customHooks/useAuth.ts
@@ -1,47 +1,105 @@
-import {useContext} from "react";
+import {useContext, useState} from "react";
 import {AuthContext} from "../context/AuthProvider";
 import {AuthApi} from "../network/AuthApi";
+import {useMutation} from "react-query";
+import {JwtDto, SuccessResponseApi, UserDto} from "@aindo/dto";
+import {AxiosError} from "axios";
 
 export function useAuth() {
     const context = useContext(AuthContext);
+    const [authError, setAuthError] = useState<AxiosError | undefined>(undefined);
 
-    const signup = async (name: string, password: string, email: string) => {
-        return AuthApi.signUp({ name, password, email })
-            .then((res) => {
-            context.setAuth({
-                user: { name },
-                token: res.data!.token,
-                expiresIn: res.data!.expiresIn,
-            });
-            sessionStorage.setItem(
-                "authState",
-                JSON.stringify({
-                    user: { name },
-                    token: res.data!.token,
-                    expiresIn: res.data!.expiresIn,
-                })
-            );
-        });
-    };
+    const {
+        mutateAsync: signup,
+        isLoading: isSignupLoading,
+    } = useMutation<SuccessResponseApi<JwtDto>, AxiosError, Omit<UserDto, "_id">>(
+        async (user) => AuthApi.signUp(user),
+        {
+            onSuccess: (response, variables) => {
+                context.setAuth({
+                    user: { name: variables.name },
+                    token: response.data.token,
+                    expiresIn: response.data.expiresIn,
+                });
+                sessionStorage.setItem(
+                    "authState",
+                    JSON.stringify({
+                        user: { name: variables.name },
+                        token: response.data.token,
+                        expiresIn: response.data.expiresIn,
+                    })
+                );
+            },
+            onError: (signupError) => {
+                setAuthError(signupError);
+            },
+        }
+    );
 
-    const login = async (name: string, password: string) => {
-        return AuthApi.login({ name, password })
-            .then((res) => {
-            context.setAuth({
-                user: { name },
-                token: res.data!.token,
-                expiresIn: res.data!.expiresIn,
-            });
-            sessionStorage.setItem(
-                "authState",
-                JSON.stringify({
-                    user: { name },
-                    token: res.data!.token,
-                    expiresIn: res.data!.expiresIn,
-                })
-            );
-        });
-    };
+    const {
+        mutateAsync: login,
+        isLoading: isLoginLoading,
+    } = useMutation<SuccessResponseApi<JwtDto>, AxiosError, Omit<UserDto, "email">>(
+        async (user) => AuthApi.login(user),
+        {
+            onSuccess: (response, variables) => {
+                context.setAuth({
+                    user: { name: variables.name },
+                    token: response.data.token,
+                    expiresIn: response.data.expiresIn,
+                });
+                sessionStorage.setItem(
+                    "authState",
+                    JSON.stringify({
+                        user: { name: variables.name },
+                        token: response.data.token,
+                        expiresIn: response.data.expiresIn,
+                    })
+                );
+            },
+            onError: (loginError) => {
+                setAuthError(loginError);
+            },
+        }
+    );
+
+    // const signup = async (name: string, password: string, email: string) => {
+    //     return AuthApi.signUp({ name, password, email })
+    //         .then((res) => {
+    //         context.setAuth({
+    //             user: { name },
+    //             token: res.data!.token,
+    //             expiresIn: res.data!.expiresIn,
+    //         });
+    //         sessionStorage.setItem(
+    //             "authState",
+    //             JSON.stringify({
+    //                 user: { name },
+    //                 token: res.data!.token,
+    //                 expiresIn: res.data!.expiresIn,
+    //             })
+    //         );
+    //     });
+    // };
+    //
+    // const login = async (name: string, password: string) => {
+    //     return AuthApi.login({ name, password })
+    //         .then((res) => {
+    //         context.setAuth({
+    //             user: { name },
+    //             token: res.data!.token,
+    //             expiresIn: res.data!.expiresIn,
+    //         });
+    //         sessionStorage.setItem(
+    //             "authState",
+    //             JSON.stringify({
+    //                 user: { name },
+    //                 token: res.data!.token,
+    //                 expiresIn: res.data!.expiresIn,
+    //             })
+    //         );
+    //     });
+    // };
 
     const logout = () => {
         context.setAuth(undefined);
@@ -51,8 +109,11 @@ export function useAuth() {
     return {
         isAuthenticated: !!context?.auth?.token,
         user: context?.auth?.user,
+        isSignupLoading,
         signup,
+        isLoginLoading,
         login,
+        authError,
         logout,
     };
 }
